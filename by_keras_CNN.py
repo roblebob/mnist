bea605647db205305a9b247ce07250bf30033c63
robert koppisch <roblebob@yahoo.de>
2019-08-19 18:21:29 +0200

    preparing new start

--- by_keras_CNN.py
+++ by_keras_CNN.py
@@ -41,29 +41,23 @@ y_test = keras.utils.to_categorical(y_test, num_category)
 
 
 
-
+########################################################################################################################
 #reshaping
 #this assumes our data format
 #For 3D data, "channels_last" assumes (conv_dim1, conv_dim2, conv_dim3, channels) while
 #"channels_first" assumes (channels, conv_dim1, conv_dim2, conv_dim3).
-
-img_rows = X_train.shape[-2]
-img_cols = X_train.shape[-1]
-
-if keras.image_data_format() == 'channels_first':
-    X_train = X_train.reshape(X_train.shape[0], 1, img_rows, img_cols)
-    X_test = X_test.reshape(X_test.shape[0], 1, img_rows, img_cols)
-    input_shape = (1, img_rows, img_cols)
-else:
-    X_train = X_train.reshape(X_train.shape[0], img_rows, img_cols, 1)
-    X_test = X_test.reshape(X_test.shape[0], img_rows, img_cols, 1)
-    input_shape = (img_rows, img_cols, 1)
-
-
-# more reshaping
+#
+assert(X_train.shape[1] == X_test.shape[1] and X_train.shape[2] == X_test.shape[2])
+#
+# assuming 'channels_first':
+input_shape = (1, X_train.shape[1], X_train.shape[2])
+X_train = X_train.reshape(X_train.shape[0], 1, X_train.shape[1], X_train.shape[2])
+X_test = X_test.reshape(X_test.shape[0], 1, X_test.shape[1], X_test.shape[2])
+#
+# normalize
 X_train = X_train.astype('float32')
 X_test = X_test.astype('float32')
 X_train /= 255
 X_test /= 255
 
-print('X_train shape:', X_train.shape) # X_train shape: (60000, 28, 28, 1)
+print('X_train shape:', X_train.shape) # X_train shape: (60000, 1, 28, 28)
